79db2712f5c9611bb19a338054ed2f0c1e342369
melisamalala <melissamalala@gmail.com>
2018-07-04 11:24:11 +0300

    Added in multiplication, division and subtraction functions

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,4 @@ var add = function(number1, number2) {
       var number1 = parseInt(prompt("Enter a number:"));
       var number2 = parseInt(prompt("Enter another number:"));
        alert(add(number1, number2))
+    
